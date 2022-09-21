40209a3b7bafeb9a1aa8bfbb07096f7fe7074f9f
chen.yaoyi <152296058@qq.com>
2022-09-21 10:09:17 +0800

    feat：步行街标题添加浏览器打开按钮

--- resources/webview/bxj/main.js
+++ resources/webview/bxj/main.js
@@ -101,7 +101,7 @@
                     const read = item.read ? `阅读：${item.read}` : '';
                     const lights = item.lights ? `高亮：${item.lights}` : '';
                     const replies = item.replies ? `回复：${item.replies}` : '';
-                    str += `<li data-id="hupumoyu-module-item" class="hupumoyu-module-item" data-info=${encodeURIComponent(JSON.stringify(item))} tabindex="1">${item.title}<span data-id="hupumoyu-module-item-desc" class="hupumoyu-module-item-desc">${read} ${lights} ${replies}</span></li>`;
+                    str += `<li data-id="hupumoyu-module-item" class="hupumoyu-module-item" data-info=${encodeURIComponent(JSON.stringify(item))} tabindex="1">${item.title}<span data-id="hupumoyu-module-item-desc" class="hupumoyu-module-item-desc">${read} ${lights} ${replies}</span><a class="hupumoyu-module-item-link" title="浏览器打开" href="https://bbs.hupu.com${item.url}" target="_blank" class="hupumoyu-module-item-icon"><svg t="1663724727885" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="25457" width="16" height="16"><path d="M810.68 810.664H213.32V213.336H480V128H213.32C166.382 128 128 166.394 128 213.336v597.328C128 857.606 166.382 896 213.32 896h597.36c46.938 0 85.32-38.394 85.32-85.336V544h-85.32v266.664zM576 128v85.336h174.948L319.998 644.266l59.732 59.732 430.952-430.94V448H896V128H576z" p-id="25458" fill="#bfbfbf"></path></svg></a></li>`;
                 }
                 oModuleList.innerHTML = str;
 
